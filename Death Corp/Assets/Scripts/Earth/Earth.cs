628d1c05abe4c3af22b7711976a521fd135b2ed7
Hugo Uchôas Borges <hugoborges@unifei.edu.br>
2019-03-26 21:16:58 -0300

    [ADD] Earth Prefab + Rotação padrão

---
+++ Death Corp/Assets/Scripts/Earth/Earth.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Earth : MonoBehaviour
+{
+
+    #region Variables
+
+    [Range(1, 240)]
+    [SerializeField]
+    private int periodSeconds = 120;
+
+    #endregion
+
+    private void rotate()
+    {
+        float rpm = (60f / periodSeconds);
+        float rotationAngle = 6f * rpm * Time.deltaTime;
+
+        transform.Rotate(Vector3.forward, rotationAngle);
+    }
+
+    private void Update()
+    {
+        rotate();
+    }
+}
