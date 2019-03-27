f3eaf7f0bb694e3e09923666551638b2391489a7
Hugo Uchôas Borges <hugoborges@unifei.edu.br>
2019-03-26 21:46:23 -0300

    [ADD] Mouse input methods

--- Death Corp/Assets/Scripts/Earth/Earth.cs
+++ Death Corp/Assets/Scripts/Earth/Earth.cs
@@ -13,6 +13,39 @@ public class Earth : MonoBehaviour
 
     #endregion
 
+    #region Mouse Inputs
+
+    private void OnMouseDrag()
+    {
+        //Debug.Log("Mouse Drag");
+    }
+
+    private void OnMouseEnter()
+    {
+        //Debug.Log("Mouse Enter");
+    }
+
+    private void OnMouseExit()
+    {
+        //Debug.Log("Mouse Exit");
+    }
+
+    private void OnMouseOver()
+    {
+        //Debug.Log("Mouse Over");
+    }
+
+    private void OnMouseUp()
+    {
+        Debug.Log("Mouse Up");
+    }
+    private void OnMouseDown()
+    {
+        Debug.Log("Mouse Down");
+    }
+
+    #endregion
+
     private void rotate()
     {
         float rpm = (60f / periodSeconds);
