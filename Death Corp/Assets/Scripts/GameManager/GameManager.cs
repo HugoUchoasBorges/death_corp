808b0a7aa15084ad460801aafd6be74987517019
Hugo Uchôas Borges <hugoborges@unifei.edu.br>
2019-03-28 08:09:08 -0300

    [ADD] Simple GameManager

---
+++ Death Corp/Assets/Scripts/GameManager/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : Singleton<GameManager>
+{
+
+    #region Variables
+    
+    #endregion
+
+    /// <summary>
+    /// Method to be called when the player dies all it lifes
+    /// </summary>
+    public static void GameOver()
+    {
+        ResetGame();
+    }
+
+    ///<summary>
+    /// Resets the Game, loading an specified scene
+    ///</summary>
+    public static void ResetGame()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        ResetGame(scene);
+    }
+
+    /// <summary>
+    /// Resets the Game, loading an specified scene
+    /// </summary>
+    /// <param name="scene">Optional Scene parameter to be loaded</param>
+    public static void ResetGame(Scene scene)
+    {
+        // Loads the previous scene
+        SceneManager.LoadScene(scene.name);
+    }
+}
