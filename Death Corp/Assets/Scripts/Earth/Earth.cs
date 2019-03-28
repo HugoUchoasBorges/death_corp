5c2b1b1734f686dc76c54818fa7e270740fd79ea
Hugo Uchôas Borges <hugoborges@unifei.edu.br>
2019-03-28 13:30:08 -0300

    [REF] Earth -> GameController Refactor

--- Death Corp/Assets/Scripts/Earth/Earth.cs
+++ Death Corp/Assets/Scripts/Earth/Earth.cs
@@ -16,21 +16,7 @@ public class Earth : MonoBehaviour
     private int periodSeconds = 120;
 
     [Space(5)]
-    [Header("Game Info")]
-    [SerializeField]
-    private int clickAmount = 0;
-    [SerializeField]
-    private int soulsCollected = 0;
-    [SerializeField]
-    private int soulsCRI = 0;
-    [SerializeField]
-    private int soulsCRC = 0;
-    [SerializeField]
-    private int deathRate = 0;
-    [SerializeField]
-    private int birthRate = 0;
-    [SerializeField]
-    private int faithLevel = 0;
+    [Header("Earth Info")]
     [SerializeField]
     private int population = 0;
 
@@ -47,23 +33,10 @@ public class Earth : MonoBehaviour
         FloatingPopupController.Initialize();
     }
 
-    void CollectSoul()
-    {
-        soulsCollected++;
-        FloatingPopupController.CreateFloatingPopup();
-        //Debug.Log("Soul Collected");
-    }
-
     #region Mouse Inputs
-
-    private void OnMouseDrag()
-    {
-        //Debug.Log("Mouse Drag");
-    }
 
     private void OnMouseEnter()
     {
-        //Debug.Log("Mouse Enter");
         if (animator)
         {
             animator.SetBool("MouseEnter", true);
@@ -72,24 +45,16 @@ public class Earth : MonoBehaviour
 
     private void OnMouseExit()
     {
-        //Debug.Log("Mouse Exit");
         if (animator)
         {
             animator.SetBool("MouseEnter", false);
         }
     }
 
-    private void OnMouseOver()
-    {
-        //Debug.Log("Mouse Over");
-    }
-
     private void OnMouseUp()
     {
-        clickAmount++;
-        CollectSoul();
+        GameManager.gameControllerInstance.CollectSouls();
 
-        //Debug.Log("Mouse Up");
         if (animator)
         {
             animator.SetBool("MouseDown", false);
@@ -97,7 +62,6 @@ public class Earth : MonoBehaviour
     }
     private void OnMouseDown()
     {
-        //Debug.Log("Mouse Down");
         if (animator)
         {
             animator.SetBool("MouseDown", true);
--- Death Corp/Assets/Scripts/GameController/GameController.cs
+++ Death Corp/Assets/Scripts/GameController/GameController.cs
@@ -8,9 +8,31 @@ public class GameController : MonoBehaviour {
 
     [Header("Points")]
     [SerializeField]
+    private int clickAmount = 0;
+    [SerializeField]
+    private int soulsCollected = 0;
+    [SerializeField]
     private int blessingPoints = 0;
     [SerializeField]
     private int cursePoints = 0;
+
+    [Space(5)]
+    [Header("Game Info")]
+    [SerializeField]
+    private int soulsCRI = 0;
+    [SerializeField]
+    private int soulsCRC = 0;
+    [SerializeField]
+    private int deathRate = 0;
+    [SerializeField]
+    private int birthRate = 0;
+    [SerializeField]
+    private int faithLevel = 0;
+
+    [Space(5)]
+    [Header("PowerUps")]
+    [SerializeField]
+    private int multiplier = 1;
 
     #endregion
 
@@ -18,5 +40,24 @@ public class GameController : MonoBehaviour {
     {
         // Tells Singleton GameManager that I'm the main GameController instance  
         GameManager.gameControllerInstance = this;
+    }
+
+    /// <summary>
+    /// Collect 1 soul
+    /// </summary>
+    public void CollectSouls()
+    {
+        CollectSouls(1);
+    }
+
+    /// <summary>
+    /// Collect Souls
+    /// </summary>
+    /// <param name="amount">Amount of Souls to Collect</param>
+    public void CollectSouls(int amount)
+    {
+        FloatingPopupController.CreateFloatingPopup();
+        soulsCollected += amount * multiplier;
+        clickAmount++;
     }
 }
