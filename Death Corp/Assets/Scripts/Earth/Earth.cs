67e38cbc7f3402d9aab8d4b91611e6efae4fa692
Hugo Uchôas Borges <hugoborges@unifei.edu.br>
2019-03-30 16:32:55 -0300

    [IMP] Mouse Animation

--- Death Corp/Assets/Scripts/Earth/Earth.cs
+++ Death Corp/Assets/Scripts/Earth/Earth.cs
@@ -69,6 +69,7 @@ public class Earth : MonoBehaviour
         {
             animator.SetBool("MouseDown", false);
         }
+        GameManager.mousePointerInstance.Click();
     }
     private void OnMouseDown()
     {
--- Death Corp/Assets/Scripts/GameController/MousePointer.cs
+++ Death Corp/Assets/Scripts/GameController/MousePointer.cs
@@ -2,13 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MousePointer : MonoBehaviour {
+public class MousePointer : MonoBehaviour
+{
 
+    #region Variables
+
+    public Animator animator;
+
+    #endregion
 
     private void Awake()
     {
         GameManager.mousePointerInstance = this;
         Cursor.visible = false;
+
+        animator = gameObject.GetComponent<Animator>();
+    }
+
+    public void Click()
+    {
+        if (animator)
+        {
+            if(animator.GetBool("MouseClick") == true)
+            {
+                animator.Play("MouseClick", -1, 0f);
+            }else
+            {
+                animator.SetBool("MouseClick", true);
+            }
+        }
+    }
+
+    public void UnClick()
+    {
+        if (animator)
+        {
+            animator.SetBool("MouseClick", false);
+        }
     }
 
     private void Update()
